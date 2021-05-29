6001f857ab746f8de0bcf647cd2545815611e311
mjmmattoni98 <javier.magni@gmail.com>
2021-05-29 22:52:06 +0200

    Servidor corregido

--- backendFirebase/functions/src/index.ts
+++ backendFirebase/functions/src/index.ts
@@ -1,6 +1,5 @@
 import * as functions from "firebase-functions"
 import * as admin from "firebase-admin"
-// const serviceAccount = require("C:\\Users\\javie\\Documents\\project-domotica\\backendFirebase\\service-account-key.json")
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
@@ -24,9 +23,16 @@ export const devicesStates = functions.region('europe-west1').firestore
         return null
       }
       
-      const states = ["open", "motion_detected", "on"]
-      
-      if(states.includes(after.estado.toLowerCase())){
+      const statesActive = ["open", "motion_detected"] // Estados activos de los dispositivos que no son de tipo alarma
+      // const statesInactive = ["close", "no_motion"] // Estados activos de los dispositivos que no son de tipo alarma
+      const alarmaActivada = "on" // Estado en el que está la alarma cuando está activada
+      const alarmaDesactivada = "off" // Estado en el que está la alarma cuando está desactivada
+
+      const nuevoEstadoDispositivo: string = after.estado.toLowerCase()
+      if(alarmaActivada === nuevoEstadoDispositivo){
+        // Si se ha activado una alarma, comprobar que se encuentra en una habitación, e indicar 
+        // que hay una alarma activada en la habitación
+
         const refHabitacion = await db
           .collection('habitaciones')
           .doc(after.habitacion)
@@ -43,20 +49,47 @@ export const devicesStates = functions.region('europe-west1').firestore
           refHabitacion.ref.update({
             activo: true
           })
+
+          const tokens = await getTokens(after.uid)
+          
+          const payload = {
+            notification: {
+              title: "Se ha activado una alarma",
+              body: `Se ha activado la alarma ${after.nombre} de la habitacion ${dataHabitacion.nombre}`,
+              click_action: 'FLUTTER_NOTIFICATION_CLICK'
+            },
+            data: {
+              idDispositivo: after.id,
+              idHabitacion: after.habitacion
+            }
+          }
+  
+          return await fcm.sendToDevice(tokens, payload, {
+            // Required for background/quit data-only messages on iOS
+            // contentAvailable: true,
+            // Required for background/quit data-only messages on Android
+            priority: "high",
+          })
+          .then(() => console.log("Usuario avisado de activacion de alarma"))
+          .catch(err => console.log("Error avisando al usuario de activacion de alarma: ", err))
         }
-      }else{
+      }else if(alarmaDesactivada === nuevoEstadoDispositivo){
+        // Si se ha desactivado una alarma, comprobar que se encuentra en una habitación, e indicar 
+        // que ya no hay una alarma activada en la habitación, si fuera necesario 
+
         let ultimo: boolean = false
+        // Coger todos los dispositivos de la habitación del usuario que sean del tipo alarma y estén activadas
         await db.collection('dispositivos')
-          .where('uid', '==', after.uid)
           .where('habitacion', '==', after.habitacion)
           .where('tipo', '==', 'alarma')
+          .where('estado', '==', 'on')
           .get()
           .then((snapshot) => {
-            if(snapshot.size == 1){
+            if(snapshot.empty){
               ultimo = true
             }
           }).catch(err => {
-            console.log("Algo ha salido mal viendo los dispositivos en una habitacion ", err)
+            console.log("Algo ha salido mal viendo las alarmas activadas en la habitacion ", err)
           })
         
         if(ultimo){
@@ -78,94 +111,66 @@ export const devicesStates = functions.region('europe-west1').firestore
             })
           }
         }
-      }
+      }else if (statesActive.includes(nuevoEstadoDispositivo)) {
+        // Se ha activado un dispositivo de tipo movimiento o apertura
 
-      if (after.estado.toLowerCase() === "disconnected" || states.includes(after.estado.toLowerCase())) {
-        const tokens = await getTokens(after.uid)
+        const idHabitacion: string = after.habitacion
+        cambiarEstadoAlarma(idHabitacion, true)
+      }else{ // statesInactive.includes(nuevoEstadoDispositivo) || nuevoEstadoDispositivo === "disconnected"
+        // Se ha desactivado o desconectado un dispositivo
 
-        const refHabitacion = await db
-          .collection('habitaciones')
-          .doc(after.habitacion)
+        let ultimo: boolean = false
+        // Coger todos los dispositivos de la habitación del usuario que no sean del tipo alarma y estén activados
+        await db.collection('dispositivos')
+          .where('habitacion', '==', after.habitacion)
+          .where('tipo', '!=', 'alarma')
+          .where('estado', 'in', statesActive)
           .get()
-
-        const dataHabitacion = refHabitacion.data()
-
-        if(dataHabitacion === undefined) {
-          console.log("La habitacion no existe")
-          return null
-        }
-
-        let payload: admin.messaging.MessagingPayload
-        if (after.estado.toLowerCase() === "disconnected") {
-          payload = {
-            notification: {
-              title: "DISPOSITIVO DESCONECTADO!!!!!!!",
-              body: `Se ha desconectado el dispositivo ${after.nombre} de la habitacion ${dataHabitacion.nombre}`,
-              click_action: 'FLUTTER_NOTIFICATION_CLICK'
-            },
-            data: {
-              idDispositivo: after.id,
-              idHabitacion: ""
+          .then((snapshot) => {
+            if(snapshot.empty){
+              ultimo = true
             }
-          }
+          }).catch(err => {
+            console.log("Algo ha salido mal viendo los dispositivos activados en la habitacion ", err)
+          })
+        
+        if(ultimo){
+          // Si no quedan dispositivos activos en la habitación, desconectamos la alarma si hubiera
+        
+          const idHabitacion: string = after.habitacion
+          cambiarEstadoAlarma(idHabitacion, false)
         }
-        else {
-          if(after.tipo.toLowerCase() !== "alarma"){
-            const idHabitacion = after.habitacion
-            const uidDispositivo = after.uid
-            activaAlarmaSiHay(idHabitacion, uidDispositivo)
-          }
-          payload = {
-            notification: {
-              title: "Se ha activado un dispositivo",
-              body: `Se ha activado el dispositivo ${after.nombre} de la habitacion ${dataHabitacion.nombre}`,
-              click_action: 'FLUTTER_NOTIFICATION_CLICK'
-            },
-            data: {
-              idDispositivo: after.id,
-              idHabitacion: after.habitacion
-            }
-          }
-        }
-
-        return await fcm.sendToDevice(tokens, payload, {
-          // Required for background/quit data-only messages on iOS
-          // contentAvailable: true,
-          // Required for background/quit data-only messages on Android
-          priority: "high",
-        })
-        .then(() => console.log("Usuario avisado de activacion de dispositivo"))
-        .catch(err => console.log("Error avisando al usuario de activacion de dispositivo: ", err))
       }
+
       return null
     })
 
-async function activaAlarmaSiHay(idHabitacion: string, uidDispositivo: string): Promise<void> {
-  const refHabitacion = await db.collection('habitaciones').doc(idHabitacion).get()
-  const dataHabitacion = refHabitacion.data()
-
-  if(dataHabitacion !== undefined){
-    await db.collection('dispositivos')
-      .where('uid', '==', uidDispositivo)
-      .where('habitacion', '==', idHabitacion)
-      .where('tipo', '==', 'alarma')
-      .get()
-      .then((snapshot) => {
-        if(snapshot.empty){
-          console.log("No hay alarmas para activar en la habitacion")
-          return
+async function cambiarEstadoAlarma(idHabitacion: string, activar: boolean): Promise<void> {
+  await db.collection('dispositivos')
+    .where('habitacion', '==', idHabitacion)
+    .where('tipo', '==', 'alarma')
+    .get()
+    .then((snapshot) => {
+      if(snapshot.empty){
+        console.log("No hay alarmas para cambiar el estado en la habitacion")
+        return
+      }
+      snapshot.forEach((doc) => {
+        let nuevoEstado = "off"
+        let viejoEstado = "on"
+        if(activar){
+          nuevoEstado = "on"
+          viejoEstado = "off"
         }
-        snapshot.forEach((doc) => {
-          const data = doc.data()
-          if(data.estado.toLowerCase() === "off"){
-            doc.ref.update({estado: "on"})
-            console.log("Alarma activada")
-          }
-        })
-      }).catch(err => {
-        console.log("Algo ha salido mal al activar las alarmas ", err)
+        const data = doc.data()
+        if(data.estado.toLowerCase() === viejoEstado){
+          doc.ref.update({estado: nuevoEstado})
+          console.log(`Alarma ${activar ? "activada" : "desactivada"}`)
+        }
       })
-  }
+    }).catch(err => {
+      console.log("Algo ha salido mal al cambiar el estado de las alarmas ", err)
+    })
 }
 
 async function getTokens(uid: string): Promise<string[]>{
